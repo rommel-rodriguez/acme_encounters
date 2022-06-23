8e69f776456553f901266f8d371bac330bc7e4d3
Rommel Rodriguez Perez <alrodperez@gmail.com>
2022-06-23 16:06:47 -0500

    Added check for expect package

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if ! which expect; then
+    echo "expect package not installed!"
+    echo "On Ubuntu based systems run:"
+    echo "> apt install expect"
+    exit 1
+fi
+
 for file in test_files/*; do
     echo "==========> TESTING ${file} <=========="
     expect "test.exp" "${file}"

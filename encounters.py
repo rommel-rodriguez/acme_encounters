26a6901a5b2d8e5aea0ccd225b58be3738e2a82e
Rommel Rodriguez Perez <alrodperez@gmail.com>
2022-06-22 21:29:31 -0500

    Finished the basic skeleton
    Basic test harness and a working script have been implemented.
    Works as expected in the sample date but need more testing and the
    creation of some dummy test files as inputs.
    Need to further optimize the algorithm.

--- encounters.py
+++ encounters.py
@@ -1,96 +1,17 @@
 #!/usr/bin/env python3
 
-"""
+""" Counter of encounters of pairs of employees in the office
+
+Prompts the user for input from the standard input and prints the table
+of number of encounters by each pair of employees.
+The file must conform to the format:
+    RENE=MO10:15-12:00,TU10:00-12:00,TH13:00-13:15,SA14:00-18:00,SU20:00-21:00
+where the first field is the name delimited by a =, followed by at most
+seven time ranges identified by the day of week first 2 letters and the format
+of the hours must follow the military style(e.g. 18:00 instead of 6p.m.). 
 """
 # TODO: Disclaimer, not taking into account more than one turn per day of the
 # week yet.
-
-
-class Turn:
-    """
-    """
-    def __init__(self, start_hour, start_minute, end_hour, end_minute):
-        self.start_hour= start_hour
-        self.start_minute = start_minute
-        self.end_hour= end_hour
-        self.end_minute = end_minute
-
-    def is_overlap(self, other_turn):
-        """ Checks if turns overlap based in hour and minute only
-        other_turn - A Turn object
-        """
-        pass
-        
-
-# TODO: Should I include the employee name in ScheduleEntry and do away with
-# Employee and encounters classes?  
-class ScheduleEntry:
-    """ Represents the day and time period a user checked in
-    """
-    def __init__(self, dow, turn):
-        """
-        dow - Day of Week
-        turn - a turn object
-        """
-        self.dow = dow
-        self.turn = turn
-
-    def is_encounter(self, sentry):
-        """
-        sentry - A ScheduleEntry type object
-        """
-        # TODO: Needs to handle military time and consider boundary cases like:
-        # 00:00 or not as the cross from one day to the other.
-        # TODO Handle repeated entries from the same employee?
-        # Need to include the employee's name in this class to do so
-        return (self.dow == sentry.dow) and self.turn.is_overlap(sentry.turn)
-
-# class EmployeeSchedule():
-#     """ Per-employee Weekly Schedule
-#     """
-#     def __init__(self, name):
-#         self.name = name
-#         self.schedule = []
-# 
-#     def register_entry(self, entry):
-#         """
-#         entry - ScheduleEntry object
-#         """
-#         pass
-
-
-class EmployeeScheduleList():
-    """ Contains all schedules of all employees 
-    """
-    def __init__(self, name):
-        self.name = name
-        # TODO: WARNING: This structure is incompatible with the rest of the design
-        # re-think and find a consensus
-        self.schedule_list = {'MO':[], 'TU':[],
-                              'WE':[], 'TH':[], 'FR':[], 'SA':[], 'SU':[]}
-
-    def register_entry(self, entry):
-        """
-        entry - ScheduleEntry object
-        """
-        pass
-
-class Employee():
-    """
-    """
-    def __init__(self,name):
-        self.name = name
-
-
-class Encounters():
-    """ Keeps track of multiple user coincidences in the office
-    Must implement an efficient algorithm to add to the value of existing keys
-    and to add new keys if necessary 
-
-    meetings - a dictionary in which the keys are pairs of strings representing
-               a pair of user names
-    """
-    pass
 
 def checkfile(file_name):
     """ Check if we can open and read a file named file_name """
@@ -110,11 +31,225 @@ def checkfile(file_name):
         return False
     return True
 
+class Turn:
+    """
+    """
+    def __init__(self, start_hour, start_minute, end_hour, end_minute):
+        """
+        start_hour - An integer number between 0 and 23
+        start_minute - An integer number between 0 and 59 
+        """
+        # TODO: Add testing for invalid hours
+        self.start_hour= start_hour
+        self.start_minute = start_minute
+        self.end_hour= end_hour
+        self.end_minute = end_minute
+
+    def is_overlap(self, other_turn):
+        """ Checks if turns overlap based in hour and minute only
+
+        Boundary cases do not count as overlaps, if the end hour and minute
+        of a turn coincides exactly with the start hour and minute of another
+        turn if does not count as an overlap
+
+        other_turn - A Turn object
+        returns False if there is no overlap, and True if there is
+        """
+        # TODO: Not taking into account ilogical time frames like a case in 
+        # and employee checks-in and -out immediately
+        # Note: if's logic separated into 2 to improve readability
+        if (self.start_hour >= other_turn.end_hour and
+            self.start_minute >= other_turn.end_minute):
+            return False
+        elif (self.end_hour <= other_turn.start_hour and
+            self.end_minute <= other_turn.start_minute):
+            return False
+        return True
+
+    def __str__(self):
+        # msg = f"{self.start_hour}:{self.start_minute}-{self.end_hour}:{self.end_minute}"
+        msg = "{self.start_hour}:{self.start_minute}-{self.end_hour}:{self.end_minute}"
+        return  msg.format(self = self)
+        
+class Employee():
+    """
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+# TODO: Should I include the employee name in ScheduleEntry and do away with
+# Employee and encounters classes?  
+class ScheduleEntry:
+    """ Represents a single employee's turn in a week
+    Characterized by an employee object, day of the week and the time frame
+    in which he/she was at the office
+    """
+    def __init__(self, emp, dow, turn):
+        """
+        emp - A Employee object
+        dow - Day of Week as a two-character string
+        turn - a Turn object
+        """
+        self.emp = emp
+        self.dow = dow
+        self.turn = turn
+
+    def is_encounter(self, sentry):
+        """
+        sentry - A ScheduleEntry type object
+        """
+        # TODO: Needs to handle military time and consider boundary cases like:
+        # 00:00 or not as the cross from one day to the other.
+        # TODO Handle repeated entries from the same employee?
+        # Need to include the employee's name in this class to do so
+        if self.emp == sentry.emp:
+            return False
+        # TODO: Depending on the logic consider removing the dow to dow test
+        return self.dow == sentry.dow and self.turn.is_overlap(sentry.turn)
+
+    def __str__(self):
+        return "{self.emp}={self.dow}{self.turn}".format(self = self)
+
+
+class EmployeeEncountersParser():
+    """ Contains all schedules of all employees 
+    """
+    def __init__(self, file_name):
+        # TODO: WARNING: This structure is incompatible with the rest of the design
+        # re-think and find a consensus
+        self.entry_dict = {'MO':[], 'TU':[],
+                           'WE':[], 'TH':[],
+                           'FR':[], 'SA':[],
+                           'SU':[]}
+
+        self.file_name = file_name
+        self.table = {}
+
+    def _parse_schedule_string(self, sched_str):
+        """ Parses a string in a know format and outputs a list of tuples
+
+        Returns a list of tuples, each with five elements representing
+         (dow, start_hour, start_minute, end_hour, end_minute ) ->
+         (str, int, int, int ,int)
+        """
+        sched_list = []
+        turns_list = sched_str.strip().split(',')
+        # sched_list = [ [(t[:2],ss, es ) for t in turns_list] for  ]
+        # At most 7 loops here
+        # TODO: Try to apply some more List Comprehensions here
+        for t in turns_list:
+            tup = tuple()
+            dow = t[:2]
+            tup += (dow, )
+            hours_list = t[2:].strip().split('-')
+            # start_list = hours_list[0].strip().split(':')
+            # end_list = hours_list[1].strip().split(':')
+            # TODO: Careful here
+            start_list, end_list = (h.strip().split(':') for h in hours_list)
+            # print(dow)
+            # print(start_list)
+            # print(end_list)
+            # TODO: Handle Exceptions in case h can not be casted
+            tup += (int(start_list[0]), int(start_list[1]))
+            tup += (int(end_list[0]), int(end_list[1]))
+            # TODO: Test which one is more efficient
+            # tup += (int(s) for s in start_list)
+            # tup += (int(s) for s in end_list)
+            # print(tup)
+            sched_list.append(tup)
+
+        return sched_list
+
+
+    def _parse_input_file(self):
+        """ Takes care of the initial parsing of the input file
+        Already assumes that the file exists and is readable
+        """
+        with open(self.file_name, 'rb') as sched_file:
+            for emp_line in sched_file: 
+                emp_line = emp_line.strip().decode('utf8')
+                emp_name, sched_str = emp_line.split('=')
+                emp_name = emp_name.upper()
+                emp = Employee(emp_name)
+                sched_list  = self._parse_schedule_string(sched_str)
+                # At most 7 loops
+                for tf in sched_list:
+                    dow = tf[0]
+                    emp_turn = Turn(*tf[1:])
+                    entry = ScheduleEntry(emp, dow, emp_turn)
+                    # print(entry)
+                    # TODO: WARNING: This is the problem, this loop is at worst
+                    # of order n, making the total O(n^2)
+                    # TODO: First Compare with entries in same day, the append
+                    # for ent in self.entry_dict[dow]:
+                    #     other_turn = ent.turn
+                    #     if emp_turn.is_overlap(other_turn):
+                    #         # TODO: Add logic to handle name1-name2, name2-name1
+                    #         # as equal in the dictionary and only use one form
+                    #         pass
+                    #     pass
+                    # TODO: WARNING: Too many nested loops, try to move some out
+                    self.entry_dict[dow].append(entry)
+
+    def register_entry(self, entry):
+        """
+        entry - ScheduleEntry object
+        """
+        pass
+
+    def enter_new_file(self, new_file_name):
+        """ Takes care of processing more than 1 input file"""
+        self.file_name = new_file_name
+        self.generate_table()
+
+    def generate_table(self):
+        """ Generates the output table and assigns it to attribute table """
+        # TODO: Must support already existing table
+        # TODO: Consider prepending _ to the parse_input_file method
+        self._parse_input_file()
+        # print([ (str(i),str(j)) for i,j in  self.entry_dict.items()]) 
+        # print(self.entry_dict)
+        # for i,j in self.entry_dict.items(): 
+        #     for it in j:
+        #         print(it)
+            
+        # TODO: WARNING: This is the problem, this loop is at worst
+        # of order n, making the total O(n^2)
+        # TODO: First Compare with entries in same day, the append
+        for dow in self.entry_dict.keys():
+            dow_elist = self.entry_dict[dow] # This shouldn't generate a copy
+            # print(dow_elist)
+            dl = len(dow_elist)
+            for i in range(dl-1) :
+                cur_ent = dow_elist[i]
+                for j in range(i+1, dl):
+                    test_ent = dow_elist[j]
+                    if cur_ent.is_encounter(test_ent):
+                        # print(f"cur_ent:{cur_ent}\ntest_ent:{test_ent}")
+                        # print(type(cur_ent.emp.name))
+                        # print(cur_ent.emp.name)
+                        # print(test_ent.emp.name)
+                        pair_list = [cur_ent.emp.name, test_ent.emp.name]
+                        pair_list.sort()
+                        pair_key = tuple(pair_list)
+                        self.table[pair_key] = self.table.get(pair_key, 0) + 1
+        # TODO: WARNING: Too many nested loops, try to move some out
+
+
+
 def main():
-    file_name = input('Enter Employee schedule file: ')
+    file_name = input('Enter Employee schedule file: ').strip()
 
     if checkfile(file_name):
-        pass
+        parser = EmployeeEncountersParser(file_name)
+        parser.generate_table()
+        print(parser.table)
     else:
         print(f"Terminating Process due to problems reading the file: {file_name}")
 
--- test_encounters.py
+++ test_encounters.py
@@ -5,7 +5,34 @@ import encounters
 
 class EncountersTestCase(unittest.TestCase):
     def setUp(self):
-       self.en = encounters
+        emp_name1='CHARLES'
+        emp_name2='MARIAN'
+        emp_name3='EDWARD'
+        emp_name4='WENDY'
+        self.en = encounters
+        self.a_turn = self.en.Turn(20,30,21,45)
+        self.b_turn = self.en.Turn(20,25,20,30)
+        self.c_turn = self.en.Turn(21,30,22,30)
+        self.d_turn = self.en.Turn(20,30,21,45)
+        self.e_turn = self.en.Turn(18,30,19,00)
+        self.emp1 = self.en.Employee(emp_name1)
+        self.emp2 = self.en.Employee(emp_name2)
+        self.emp3 = self.en.Employee(emp_name3)
+        self.entry1 = self.en.ScheduleEntry(self.emp1, 'MO', self.a_turn)
+        self.entry2 = self.en.ScheduleEntry(self.emp1, 'TU', self.b_turn)
+        self.entry3 = self.en.ScheduleEntry(self.emp2, 'MO', self.b_turn)
+        self.entry4 = self.en.ScheduleEntry(self.emp3, 'MO', self.c_turn)
+        self.sched_string1 = 'TU10:30-11:45,TH01:30-02:00,SU21:30-23:59'
+        self.sched_string2 = 'MO9:30-10:00,TH05:30-06:00,SU21:30-23:59'
+        self.expected_list1 = [('TU', 10, 30, 11, 45),('TH', 1,30,2,0),
+                               ('SU',21,30,23,59)]
+        self.expected_list2 = [('MO', 9, 30, 10, 0),('TH', 5,30,6,0),
+                               ('SU',21,30,23,59)]
+        self.expected_dict1 = {'MO':[self.entry1, self.entry3, self.entry4],
+                               'TU':[self.entry4],
+                               'WE':[], 'TH':[],
+                               'FR':[], 'SA':[],
+                               'SU':[]}
 
     def test_checkfile(self):
         """ Sets the filenames to test the checkfile function
@@ -20,6 +47,39 @@ class EncountersTestCase(unittest.TestCase):
         fnot_exists = "./non_existent.txt"
         self.assertTrue(self.en.checkfile(f_exists))
         self.assertFalse(self.en.checkfile(fnot_exists))
+
+    def test_is_overlap(self):
+        """ Tests the Turn.is_overlap method """
+        self.assertFalse(self.a_turn.is_overlap(self.b_turn))
+        self.assertTrue(self.a_turn.is_overlap(self.c_turn))
+        self.assertTrue(self.a_turn.is_overlap(self.d_turn))
+        self.assertFalse(self.a_turn.is_overlap(self.e_turn))
+
+    def test_is_encounter(self):
+        """ Tests the ScheduleEntry.is_encounter method"""
+        self.assertFalse(self.entry1.is_encounter(self.entry2))
+        self.assertFalse(self.entry1.is_encounter(self.entry3))
+        self.assertTrue(self.entry1.is_encounter(self.entry4))
+
+    def test_parse_schedule_string(self):
+        """ Tests the EmployeeeEncounterParser.test_parse_schedule_string method
+        """
+        parser = self.en.EmployeeEncountersParser('dummy_file.txt')
+        self.assertListEqual(self.expected_list1,
+                             parser._parse_schedule_string(self.sched_string1))
+        self.assertListEqual(self.expected_list2,
+                             parser._parse_schedule_string(self.sched_string2))
+
+    def test_parse_input_file(self):
+        """ Tests the EmployeeeEncounterParser.test_parse_input_file """
+        parser1 = self.en.EmployeeEncountersParser('dummy_input1.txt')
+        parser1._parse_input_file()
+        # parser2 = self.en.EmployeeEncountersParser('dummy_input2.txt')
+        self.assertDictEqual(self.expected_dict1, parser1.entry_dict)
+
+    def test_generate_table(self):
+        """ """
+        pass
 
 
 if __name__ == '__main__':
